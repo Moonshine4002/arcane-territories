d052f2661335c7fa1ec5301118a18ecd4ad81293
Moonshine4002 <moonshine2004@163.com>
2025-02-10 13:19:40 +0800

    major refactor

--- map/kenney_tiny-dungeon/level_1.gd
+++ map/kenney_tiny-dungeon/level_1.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 @export var player_scene: PackedScene
 
-# texture
+# tile map
 var source_id = 0
-@onready var character_tile: TileMapLayer = $Character
 @onready var ground_tile: TileMapLayer = $Ground
 @onready var rail_tile: TileMapLayer = $Rail
+@onready var character_tile: TileMapLayer = $Character
 @onready var object_tile: TileMapLayer = $Object
-@onready var tile_set = character_tile.tile_set
+@onready var weapon_tile: TileMapLayer = $Weapon
+@onready var tile_set = ground_tile.tile_set
 @onready var atlas_source: TileSetAtlasSource = tile_set.get_source(source_id)
 
 # save
@@ -25,7 +26,7 @@ var character_data = {
 		"level": 10,
 		"experience": 0,
 		"mana": 10,
-		"speed": 1.2,
+		"speed": 10,
 		"dodge": 10,
 	},
 	"Player9":
@@ -37,7 +38,7 @@ var character_data = {
 		"level": 10,
 		"experience": 0,
 		"mana": 100,
-		"speed": 1.0,
+		"speed": 5,
 		"dodge": 30,
 	},
 	"Enemy9":
@@ -49,7 +50,7 @@ var character_data = {
 		"level": 1,
 		"experience": 0,
 		"mana": 1,
-		"speed": 0.8,
+		"speed": 3.3,
 		"dodge": 10,
 	},
 }
@@ -64,106 +65,142 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	for player in get_tree().get_nodes_in_group("Player"):
-		if player.moving:
-			continue
-		var previous_position = player.cell_position
-
-		# calculate tween duration
-		var tile_data = object_tile.get_cell_tile_data(previous_position)
-		if not tile_data:
-			tile_data = rail_tile.get_cell_tile_data(previous_position)
-		if not tile_data:
-			tile_data = ground_tile.get_cell_tile_data(previous_position)
-		var speed = tile_data.get_custom_data("speed") * player.data["speed"]
-		player.calculate_tween_duration(speed)
-
-		# control
-		var target_cell = control(player, tile_data)
-		player.set_cell_position(target_cell)
-		var present_position = player.cell_position
-		control_cart(tile_data, previous_position, present_position)
+	pass
 
 
-func control(player, tile_data) -> Vector2:
-	var target_cell
-	target_cell = player.control()
-
-	# rail
-	var previous_position = player.cell_position
-	if player.pressed[player.xy_index] and tile_data.get_custom_data("name") == "cart":
-		var rail_data = rail_tile.get_cell_tile_data(previous_position)
-		if rail_data and "rail" in rail_data.get_custom_data("name"):
-			var available_input = []
-			for dir in player.inputs:
-				if dir in rail_data.get_custom_data("control"):
-					available_input.append(player.inputs[dir])
-			available_input.erase(-player.previous_component)
-			if available_input.size() != 1:
-				return target_cell
-			var input = available_input[0]
-			target_cell = player.cell_position + input
-	return target_cell
-
-
-func control_cart(tile_data, previous_position, present_position) -> void:
-	if previous_position == present_position:
-		return
-	if tile_data.get_custom_data("name") == "cart":
-		var rail_data = rail_tile.get_cell_tile_data(present_position)
-		var object_data = object_tile.get_cell_tile_data(present_position)
-		if not rail_data:
-			return
-		if rail_data.get_custom_data("name") == "rail_straight":
-			if not object_data or object_data.get_custom_data("name") != "cart":
-				object_tile.erase_cell(previous_position)
-			object_tile.set_cell(present_position, source_id, Vector2(6, 4))
-		elif rail_data.get_custom_data("name") == "rail_bent":
-			if not object_data or object_data.get_custom_data("name") != "cart":
-				object_tile.erase_cell(previous_position)
-			object_tile.set_cell(present_position, source_id, Vector2(8, 4))
-
-
-func init_player(cell):
-	var tile_data = character_tile.get_cell_tile_data(cell)
-	var name = tile_data.get_custom_data("name")
-
+func init_player(cell: Vector2):
 	var player = player_scene.instantiate()
 
-	var texture := AtlasTexture.new()
-	texture.atlas = atlas_source.texture
-	texture.region = Rect2(
-		character_tile.get_cell_atlas_coords(cell) * character_tile.tile_set.tile_size,
-		character_tile.tile_set.tile_size,
-	)
-	player.get_node("Sprite2D").texture = texture
-	player.get_node("Sprite2D").scale = Vector2.ONE
-
+	var tile_data = character_tile.get_cell_tile_data(cell)
+	var name = tile_data.get_custom_data("name")
 	player.name = name
+
 	var data = load_game(path)
 	if data.has(name):
 		player.data = data[name]
 	else:
-		player.data = character_data[name]
+		return
+		#player.data = character_data[name]
 
-	player.connect("ray_cast_area", _on_source_ray_cast_target)
+	player.data["cell"] = cell
+	player.data["cell_atlas_coords"] = character_tile.get_cell_atlas_coords(cell)
+
+	player.connect_controller(_on_controller_player_request)
 
 	add_child(player)
 
-	player.set_cell_position(Vector2(cell))
+
+func _on_controller_player_request(source: Node, type: String, parameters: Array) -> void:
+	var player = source
+	match type:
+		"get_control":
+			var fun = parameters[0]
+			var moving = parameters[1]
+			if moving:
+				return
+			fun.call()
+		"move":
+			var fun = parameters[0]
+			var component = parameters[1]
+			var cell = parameters[2]
+			assert(component != Vector2.ZERO)
+			if not component:  # _ready
+				fun.call(player.data["cell"], cell)
+				return
+
+			var input_para = parameters[3]
+			var previous_displacement = input_para["previous_displacement"]
+			var pressed = input_para["pressed"]
+			var shift = input_para["shift"]
+
+			var tile_data = control_get_tile_data(player, cell)
+			var speed = tile_data.get_custom_data("speed")
+			speed *= player.data["speed"]
+			if shift:
+				speed *= 0.3
+
+			component = control_rail(
+				player, cell, tile_data, component, previous_displacement, pressed
+			)
+
+			fun.call(component, 1 / speed)
+
+			var present_cell = cell + component
+			control_cart(tile_data, cell, present_cell)
+		"set_texture":
+			var fun = parameters[0]
+			fun.call(atlas_source.texture, player.data["cell_atlas_coords"])
+		"ray_cast":
+			var target = parameters[0].get_parent()
+			print(source, " hit ", target)
+			print("source.name = ", source.name)
+			if randf() * 100 < target.data["dodge"]:
+				print(target.name, " dodged")
+			else:
+				target.data["health"] -= source.data["attack"] - target.data["defense"]
+			print("source.data.health = ", source.data["health"])
+			if target.data["health"] <= 0:
+				print(target.name, " is defeated")
+				target.queue_free()
+		_:
+			assert(false)
 
 
-func _on_source_ray_cast_target(source, target):
-	print(source, " hit ", target)
-	print("source.name = ", source.name)
-	if randf() * 100 < target.data["dodge"]:
-		print(target.name, " dodged")
-	else:
-		target.data["health"] -= source.data["attack"] - target.data["defense"]
-	print("source.data.health = ", source.data["health"])
-	if target.data["health"] <= 0:
-		print(target.name, " is defeated")
-		target.queue_free()
+func control_get_tile_data(player: Node, cell: Vector2) -> TileData:
+	var tile_data = object_tile.get_cell_tile_data(cell)
+	if not tile_data:
+		tile_data = rail_tile.get_cell_tile_data(cell)
+	if not tile_data:
+		tile_data = ground_tile.get_cell_tile_data(cell)
+	return tile_data
+
+
+func control_rail(
+	player: Node,
+	cell: Vector2,
+	tile_data: TileData,
+	component: Vector2,
+	previous_displacement: Vector2,
+	pressed: bool,
+) -> Vector2:
+	if not pressed:
+		return component
+	if tile_data.get_custom_data("name") != "cart":
+		return component
+
+	var rail_data = rail_tile.get_cell_tile_data(cell)
+	assert(rail_data)
+	assert("rail" in rail_data.get_custom_data("name"))
+	var available_input = []
+	var rail_control = rail_data.get_custom_data("control")
+	var INPUTS = player.get_node("Controller").INPUTS  # TODO: remove
+	for dir in INPUTS.keys():
+		if dir in rail_control:
+			available_input.append(INPUTS[dir])
+	available_input.erase(-previous_displacement)
+	if available_input.size() != 1:
+		return component
+	component = available_input[0]
+
+	return component
+
+
+func control_cart(tile_data: TileData, cell: Vector2, present_cell: Vector2) -> void:
+	if cell == present_cell:
+		return
+	if tile_data.get_custom_data("name") == "cart":
+		var rail_data = rail_tile.get_cell_tile_data(present_cell)
+		var object_data = object_tile.get_cell_tile_data(present_cell)
+		if not rail_data:
+			return
+		if rail_data.get_custom_data("name") == "rail_straight":
+			if not object_data or object_data.get_custom_data("name") != "cart":
+				object_tile.erase_cell(cell)
+			object_tile.set_cell(present_cell, source_id, Vector2(6, 4))
+		elif rail_data.get_custom_data("name") == "rail_bent":
+			if not object_data or object_data.get_custom_data("name") != "cart":
+				object_tile.erase_cell(cell)
+			object_tile.set_cell(present_cell, source_id, Vector2(8, 4))
 
 
 func _notification(what: int) -> void:
@@ -207,5 +244,6 @@ func load_game(path: String) -> Dictionary:
 			)
 			continue
 		var node_data = json.data
+		assert(node_data.has("name"))
 		data_dict[node_data["name"]] = node_data
 	return data_dict
--- src/player.gd
+++ src/player.gd
@@ -1,146 +1,16 @@
-extends Area2D
-## A 2D character that moves in a grid pattern.
-##
-## Inspired: https://kidscancode.org/godot_recipes/4.x/2d/grid_movement/index.html
-## Assisted by deepseek.
-
-# position
-@export var tile_size: int = 16
-var cell_position: Vector2
-
-# speed
-@export var seconds_per_tile: float = 0.05
-@export var shift_modifier: float = 3
-var moving: bool = false
-var tween_duration: float
-
-# control
-var inputs: Dictionary = {
-	"up": Vector2.UP,
-	"down": Vector2.DOWN,
-	"left": Vector2.LEFT,
-	"right": Vector2.RIGHT,
-	"up-left": Vector2.UP + Vector2.LEFT,
-	"up-right": Vector2.UP + Vector2.RIGHT,
-	"down-left": Vector2.DOWN + Vector2.LEFT,
-	"down-right": Vector2.DOWN + Vector2.RIGHT,
-}
-var xy_index: int = 0
-var xy_components: Array[Vector2] = [
-	Vector2(1, 0),
-	Vector2(0, 1),
-]
-var previous_input: Array[Vector2] = [
-	Vector2(0, 0),
-	Vector2(0, 0),
-]
-var pressed: Array[bool] = [false, false]
-var previous_component: Vector2
-
-# collision
-@onready var ray: RayCast2D = $RayCast2D
-
-# perception
-var target_position: Vector2 = Vector2(33, 0)
-signal ray_cast_area(me, target)
+extends Node
 
 # data
 var data: Dictionary
 
 
 func _ready() -> void:
-	set_cell_position(cell_position)
+	pass
 
 
 func _process(delta: float) -> void:
-	target_position = target_position.rotated(PI / 2)
-	if scan_area(target_position):
-		ray_cast_area.emit(self, ray.get_collider())
+	pass
 
 
-func control() -> Vector2:
-	if moving:
-		return cell_position
-
-	var component: Vector2 = input()
-
-	if component == Vector2.ZERO:
-		previous_input[xy_index] = component
-		pressed[xy_index] = false
-		return cell_position
-	component = component.normalized()
-	if component == previous_input[xy_index]:
-		pressed[xy_index] = true
-	else:
-		previous_input[xy_index] = component
-		pressed[xy_index] = false
-
-	if not scan_area_and_body(component * tile_size):
-		return cell_position + component
-	return cell_position
-
-
-func input() -> Vector2:
-	var displacement: Vector2 = Vector2.ZERO
-	for dir in inputs.keys():
-		if Input.is_action_pressed(dir):
-			displacement += inputs[dir]
-	xy_index = (xy_index + 1) % 2
-	var component: Vector2 = xy_components[xy_index] * displacement
-
-	# check the other component if zero
-	if component == Vector2.ZERO:
-		component = xy_components[(xy_index + 1) % 2] * displacement
-
-	return component
-
-
-func scan_area(target_position: Vector2) -> bool:
-	ray.collide_with_areas = true
-	ray.collide_with_bodies = true
-	ray.target_position = target_position
-	ray.force_raycast_update()
-	if ray.get_collider() is TileMapLayer:
-		return false
-	if not ray.is_colliding():
-		return false
-	return true
-
-
-func scan_area_and_body(target_position: Vector2) -> bool:
-	ray.collide_with_areas = true
-	ray.collide_with_bodies = true
-	ray.target_position = target_position
-	ray.force_raycast_update()
-	if not ray.is_colliding():
-		return false
-	return true
-
-
-func get_cell_position() -> Vector2:
-	return cell_position
-
-
-func set_cell_position(cell: Vector2) -> void:
-	if cell_position == cell:
-		return
-	previous_component = cell - cell_position
-	cell_position = cell
-	move_tween((cell + Vector2.ONE / 2) * tile_size)
-
-
-func move_tween(target_position: Vector2) -> void:
-	var tween: Tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "position", target_position, tween_duration)
-	moving = true
-	#$AnimationPlayer.play(dir)
-	await tween.finished
-	moving = false
-
-
-func calculate_tween_duration(speed_modifier: float):
-	if Input.is_key_pressed(KEY_SHIFT):
-		tween_duration = seconds_per_tile * shift_modifier
-	else:
-		tween_duration = seconds_per_tile
-	tween_duration /= speed_modifier
+func connect_controller(fun: Callable) -> void:
+	$Controller.connect("player_request", fun)
